31b888f02de33776dd459d74ea9cc223fd7ba4f9
“IglesiasT” <tiglesias@fi.uba.ar>
2021-11-10 18:15:26 -0300

    Agrego otra dimension a los headers de las funciones

--- Tablero.h
+++ Tablero.h
@@ -7,31 +7,31 @@
 
 class Tablero{
 private:
-    Lista<Lista<Casillero *> *> * casilleros;   //tablero de dos dimensiones con listas
+    Lista<Lista<Lista<Casillero *> *> *> * casilleros;
 
 public:
     /*
-     * Pre: recibe las dimensiones con las que se desea crear el tablero
-     * Post: inicializa un tablero de x ancho por y alto de casilleros vacios
+     * Pre: recibe las longitudes de las tres dimensiones con las que se desea crear el tablero
+     * Post: inicializa un tablero de x ancho, y alto, z profundidad de casilleros vacios
      */
-    Tablero(unsigned int x, unsigned int y);
+    Tablero(unsigned int x, unsigned int y, unsigned int z);
 
     /*
-     *
+     * Post: libera la memoria solicitada para la creacion del tablero
      */
     ~Tablero();
 
     /*
-     * Pre: recibe las coordenadas x,y del casillero que se desea obtener
+     * Pre: recibe las coordenadas x,y,z del casillero que se desea obtener
      * Post: devuelve el casillero que se encuentra en la coordenada recibida
      */
-    Casillero * getCasillero(unsigned int x, unsigned int y);
+    Casillero * getCasillero(unsigned int x, unsigned int y, unsigned int z);
 
     /*
-     * Pre: recibe las coordenadas x,y donde se desea colocar la ficha pasada como argumento
+     * Pre: recibe las coordenadas x,y,z donde se desea colocar la ficha pasada como argumento
      * Post: se coloca la ficha recibida en el casillero de la coordenada indicada
      */
-    void setCasillero(unsigned int x, unsigned int y, Ficha * ficha);
+    void setCasillero(unsigned int x, unsigned int y, unsigned int z, Ficha * ficha);
 
     /*
      * Pre: recibe dos coordenadas pertenecientes al tablero. Las primeras indican cual
@@ -40,7 +40,7 @@ public:
      * Post: intercambia la ficha del casillero ubicado en la primer coordenada con el de la segunda
      * si las coordenadas son inválidas lanza una excepcion
      */
-    void moverFicha(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,);
+    void moverFicha(unsigned int x1, unsigned int y1, unsigned int z1, unsigned int x2, unsigned int y2, unsigned int z2);
 };
 
 #endif //TATETI_2_0_TABLERO_H
