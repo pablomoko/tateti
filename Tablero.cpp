496733ab63f6282dad99532d48bc68d719e85319
“IglesiasT” <tiglesias@fi.uba.ar>
2021-11-19 21:35:53 -0300

    Termino implementacion hayTateti

--- Tablero.cpp
+++ Tablero.cpp
@@ -65,7 +65,7 @@ bool Tablero::existeCasillero(unsigned int x, unsigned int y, unsigned int z) {
 bool Tablero::hayTateti(Casillero * casilleroOrigen){
 
     casilleroOrigen->getCasillerosAdyacentes(); //que hace esto
-    int longitudesAdyacentes[3][3][3];  //Cambiar por un nombre mas explicito
+    int longitudesAdyacentes[3][3][3];  //Cambiar por un nombre más explicito
     //En esta iteracion se guardan la cantidad de casilleros con fichas iguales en cada direccion
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -86,7 +86,8 @@ bool Tablero::hayTateti(Casillero * casilleroOrigen){
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 3; k++) {
-                if (longitudesAdyacentes[i][j][k]){
+                if ((longitudesAdyacentes[i][j][k] + 1 == 3)||    //Caso tateti siguiendo una direccion
+                    (longitudesAdyacentes[i][j][k] + longitudesAdyacentes[2-i][2-j][2-k] == 3)){   //Caso tateti en un adyacente y su opuesto
                     return true
                 }
             }
