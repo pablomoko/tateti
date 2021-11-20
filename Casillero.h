95ee2d6da96c68a92dd73142390c583a30d43dd5
“IglesiasT” <tiglesias@fi.uba.ar>
2021-11-20 16:21:53 -0300

    Agrego header tieneAdyacente

--- Casillero.h
+++ Casillero.h
@@ -4,10 +4,6 @@
 
 const char VACIO = 'Z';
 
-
-
-
-
 class Casillero{
 private:
     Ficha * ficha;
@@ -39,6 +35,13 @@ public:
      * del mismo casillero. Lanza una excepcion si las coordenadas son inválidas
      */
     void asignarCasilleroAdyacente(int x, int y, int z, Casillero * casilleroAdyacente);
+
+    /*
+     * Pre: recibe las coordenadas respecto del mismo Casillero que se desea checkear
+     * Post: devuelve true si el Casillero tiene un Casilllero adyacente existente
+     * ubicado en las coordenadas recibidas y false en caso de que no
+     */
+    bool tieneAdyacente(int x, int y, int z);
 
     /*
      * Post: devuelve la Ficha del Casillero
